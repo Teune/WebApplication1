bcd8eba6cec9a910c5745abf82e263a10e3fd602
Tony van Schaik <teune2000@hotmail.com>
2022-07-11 15:19:57 +0200

    Pizza name

--- TonysPizzeria/TonysPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ TonysPizzeria/TonysPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TonysPizzeria.Data;
+using TonysPizzeria.Models;
 
 namespace TonysPizzeria.Pages.Checkout
 {
     [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
-
         public string PizzaName { get; set; }
         public float PizzaPrice { get; set; }
 
         public string ImageTitle { get; set; }
+        private readonly ApplicationDbContext _context;
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
             if(string.IsNullOrWhiteSpace(PizzaName))
@@ -21,6 +27,14 @@ namespace TonysPizzeria.Pages.Checkout
             {
                 ImageTitle = "Create";
             }
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
+
         }
     }
 }
